17ce7e98019b24f92c1064da57bda78a287f762a
Antoineprsn <Antoineprsn@users.noreply.github.com>
2019-10-02 09:48:12 +0200

    rename score

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -1,21 +1,15 @@
 package com.example.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CompoundButton;
 import android.widget.EditText;
-import android.widget.Switch;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
     TextView monTextView;
@@ -43,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
         Button scoreButton = findViewById(R.id.buttonScore);
         scoreButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                Intent myIntent = new Intent(MainActivity.this, Score.class);
+                Intent myIntent = new Intent(MainActivity.this, ScoreActivity.class);
                 MainActivity.this.startActivity(myIntent);
             }
         });
--- app/src/main/java/com/example/myapp/ScoreActivity.java
+++ app/src/main/java/com/example/myapp/ScoreActivity.java
@@ -7,7 +7,7 @@ import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
-public class Score extends AppCompatActivity {
+public class ScoreActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,12 +15,30 @@ public class Score extends AppCompatActivity {
         setContentView(R.layout.activity_score);
 
         TableLayout maTable = (TableLayout) findViewById(R.id.tabScore);
-        TableRow row = new TableRow(this);
+        /**TableRow row = new TableRow(this);
         TextView tv = new TextView(this);
         tv.setText("This is text");
 
         maTable.addView(row);
         row.addView(tv);
+
+         table = (TableLayout)findViewById(R.id.mytable);
+
+         for(int i=0;i<data.size();i++)
+         {
+         TableRow row=new TableRow(this);
+         String phone = data.get(i);
+         String amount = data1.get(i);
+         TextView tv1=new TextView(this);
+         tv1.setText(phone);
+         TextView tv2=new TextView(this);
+         tv2.setText(amount);
+         row.addView(tv1);
+         row.addView(tv2);
+         table.addView(row);
+         }
+
+         */
 
     }
 
